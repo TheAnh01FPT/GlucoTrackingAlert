df94a23cfcee943d3ccf99d5c3f5c129e69c86a4
TheAnh01FPT <anhhthe171270@fpt.edu.vn>
2026-05-28 22:44:19 +0700

    init

---
+++ src/main/java/fpt/swp391/GlucoTrackAlert/GlucoTrackAlertApplication.java
@@ -0,0 +1,13 @@
+package fpt.swp391.GlucoTrackAlert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlucoTrackAlertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlucoTrackAlertApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fpt/swp391/GlucoTrackAlert/GlucoTrackAlertApplicationTests.java
@@ -0,0 +1,13 @@
+package fpt.swp391.GlucoTrackAlert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlucoTrackAlertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
