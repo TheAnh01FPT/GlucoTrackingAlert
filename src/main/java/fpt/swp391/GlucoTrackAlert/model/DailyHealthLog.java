160f9727ffc911aa923d2d54cd2c4f58c1ab7edd
OnLyHan <uknowhades@gmail.com>
2026-06-10 00:16:23 +0700

    Thêm mapping cho nhật ký sức khỏe ở home, fix bug sửa url là xem được log người khác, fix bug màn doctor nút sai ( chưa xong )

--- src/main/java/fpt/swp391/GlucoTrackAlert/model/DailyHealthLog.java
+++ src/main/java/fpt/swp391/GlucoTrackAlert/model/DailyHealthLog.java
@@ -22,7 +22,7 @@ public class DailyHealthLog {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "patient_id", nullable = false)
     private Patient patient;
 
