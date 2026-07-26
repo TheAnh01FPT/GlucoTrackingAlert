6bc92a525be988683131cf713e8afb8d690f17c7
MinhDuy2203 <minhduy22032006@gmail.com>
2026-07-26 21:09:49 +0700

    Resolve merge conflict and add meal log feature

--- src/main/java/fpt/swp391/GlucoTrackAlert/model/user/User.java
+++ src/main/java/fpt/swp391/GlucoTrackAlert/model/user/User.java
@@ -3,6 +3,7 @@ package fpt.swp391.GlucoTrackAlert.model.user;
 import fpt.swp391.GlucoTrackAlert.model.role.Role;
 import jakarta.persistence.*;
 import lombok.*;
+
 import java.time.LocalDateTime;
 
 @Entity
@@ -13,6 +14,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @Builder
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
